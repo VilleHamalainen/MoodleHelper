88ea0f7fcb107a33f3ac45648ecd8b59e238b659
Oskari Mäkelä <oskari.makela@hotmail.com>
2023-11-14 08:35:26 +0200

    This commit will be automatically published

---
+++ README.md
@@ -0,0 +1,3 @@
+# MoodleHelper
+
+[Linkki dokumentaatioon](https://docs.moodle.org/3x/fi/Aukkotehtävät)
---
+++ SyntaxGenerator/Models/AnswerOption.cs
@@ -0,0 +1,27 @@
+﻿namespace SyntaxGenerator.Models
+{
+    public class AnswerOption
+    {
+        public string? Text { get; set; }
+        public bool IsCorrect { get; set; }
+        public string? Feedback { get; set; }
+
+        public AnswerOption()
+        {
+            
+        }
+
+        public AnswerOption(string text, bool isCorrect)
+        {
+            Text = text;
+            IsCorrect = isCorrect;
+        }
+
+        public AnswerOption(string text, bool isCorrect, string feedback)
+        {
+            Text = text;
+            IsCorrect = isCorrect;
+            Feedback = feedback;
+        }
+    }
+}
